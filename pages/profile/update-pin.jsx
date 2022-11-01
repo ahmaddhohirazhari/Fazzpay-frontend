7cb4462f21043e65a3e4a8cb760ff1c2ddd62c00
ahmad dhohir azhari <dhohir12@gmail.com>
2022-11-01 14:58:06 +0700

    fix error

--- pages/profile/update-pin.jsx
+++ pages/profile/update-pin.jsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Layout from "../../layout";
-import PinInput from "../../components/PinInput";
+import PinInput from "../../components/pinInput";
 import axios from "../../utils/axios";
 
 export default function ChangePin() {
